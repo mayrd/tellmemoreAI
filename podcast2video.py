e1b74f068e7b8380383f4102a271138cc4e845a7
Daniel Mayr <dg.mayr@gmail.com>
2024-12-06 10:52:59 +0100

    better error handling, less verbose

--- podcast2video.py
+++ podcast2video.py
@@ -10,13 +10,16 @@ import media
 import utils
 
 def gen_yt_title(wiki_article: str) -> str:
-    return genai.genai_text(
+    response = genai.genai_text(
         f"We created a suspenseful podcast about the happenings. "
         f"Can you propose a great YouTube video title for the podcast? "
         f"Only respond with the proposed title and add some hashtags in the title. "
         f"The title should not have more than 100 characters. "
         f"This is the article content the podcast is based on: {wiki_article}"
     ).replace("\"", "")
+    if len(response) < 100:
+        return response
+    return gen_yt_title(wiki_article)
 
 def gen_short_title(wiki_article: str) -> str:
         return genai.genai_text(
@@ -24,12 +27,15 @@ def gen_short_title(wiki_article: str) -> str:
     ).replace("\"", "")
 
 def gen_yt_description(wiki_article: str) -> str:
-    return genai.genai_text(
+    response = genai.genai_text(
         f"We created a suspenseful podcast about the happenings. "
         f"Can you propose a great YouTube video description for the podcast? "
         f"The description should not have more than 5000 characters. "
         f"This is the article content the podcast is based on: {wiki_article}"
     )
+    if len(response) < 5000:
+        return response
+    return gen_yt_title(wiki_article)
 
 def gen_yt_tags(wiki_article: str) -> list[str]:
     response = genai.genai_text(
@@ -62,8 +68,6 @@ def podcast2video(folder_name: str) -> bool:
     if "yt_video_id" in md and md["yt_video_id"] is not None and len(md["yt_video_id"])>=11:
         print(f"  video already published as " + md["yt_video_id"])
         return True
-    
-    print(md)
 
     article = wiki.fetch_wiki_article(md["wiki_title"])
     if len(article)  > 60000:
@@ -100,19 +104,21 @@ def podcast2video(folder_name: str) -> bool:
         print(f"create {video_file}")
         media.podcast2video(podcast_file, os.path.join(folder_name, "img*.jpg"), video_file)
 
-    print("upload")
     md["yt_video_id"] = yt.upload_video(video_file, md["yt_title"], md["yt_description"], md["yt_tags"])
+    if md["yt_video_id"]:
+        print(f"  uploaded to YT as " + md["yt_video_id"])
+    else:
+        print(f"  upload failed. No video Id given")
 
-    print(utils.toJSON(md))
     utils.toFile(md, md_file)
-    
-    print("Done")
     return True
 
 
-# main
 if __name__ == "__main__":
     # go through the different folders in OUTPUT_FOLDER
     folders = utils.get_subfolders(os.getenv("OUTPUT_FOLDER"))
     for folder in folders:
-        podcast2video(folder)
+        try:
+            podcast2video(folder)
+        except Exception as e:
+            print(e)
