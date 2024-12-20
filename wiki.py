3eea06fc2205e1e8dc396b644330a5d10076eee0
Daniel Mayr <dg.mayr@gmail.com>
2024-12-20 11:47:28 +0100

    add wikipedia by pageid

--- wiki.py
+++ wiki.py
@@ -26,3 +26,16 @@ def fetch_wiki_article(title: str) -> str:
         content = None
 
     return content
+
+def fetch_wiki_article_by_id(pageid: str)->str:
+    try:
+        page = wikipedia.page(pageid=pageid)
+        content = page.content
+    except wikipedia.exceptions.DisambiguationError as e:
+        print("Disambiguation error:", e)
+        content = None
+    except wikipedia.exceptions.PageError as e:
+        print("Page not found:", e)
+        content = None
+
+    return content
