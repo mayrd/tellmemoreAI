d60fcc465f636f926a98c28a525a9a92127a2124
Daniel Mayr <dg.mayr@gmail.com>
2025-01-26 14:43:59 +0100

    added image manipulation

--- media.py
+++ media.py
@@ -2,7 +2,7 @@
 
 import subprocess
 import os
-
+from PIL import Image, ImageDraw, ImageFont
 
 def ffmpeg(command_args: list[str]) -> bool:
     try:
@@ -22,6 +22,30 @@ def convert(input_file: str, output_file: str) -> None:
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
 
+def image_add_text(image_file:str, text: str, font_file: str, pos_x: int, pos_y: int, font_size: int) -> bool:
+    img = Image.open(image_file)
+    d = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font_file, font_size)
+    text_color = (255, 255, 255)
+    text_position = (pos_x, pos_y)
+    d.text(text_position, text, fill=text_color, font=font)
+    img.save(image_file)
+    return True
+
+def image_add_text_centered(image_file:str, text: str, font_file: str, font_size: int, offset_x: int = 0, offset_y: int = 0) -> bool:
+    img = Image.open(image_file)
+    d = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font_file, font_size)
+    text_color = (255, 255, 255)
+    bbox = d.textbbox((0, 0), text, font=font)
+    text_width = bbox[2] - bbox[0]
+    text_height = bbox[3] - bbox[1]
+    x = (img.width - text_width) / 2
+    y = (img.height - text_height) / 2
+    d.text((x + offset_x, y + offset_y), text, fill=text_color, font=font)
+    img.save(image_file)
+    return True
+
 
 def podcast2video(podcast_audio: str, images: str, videofile: str) -> bool:
     return ffmpeg([
@@ -38,4 +62,17 @@ def podcast2video(podcast_audio: str, images: str, videofile: str) -> bool:
         "-c:a", "aac",
         "-b:a", "192k",
         videofile
+    ])
+
+def playlist2stream(videos: list[str], stream_key: str):
+    return ffmpeg([
+        #"-re","-f","concat","-safe","0",
+        "-i",videos[1],
+        #"-c:v","copy","-c:a","copy",
+        "-c:v","libx264","-preset","veryfast","-b:v","3000k","-maxrate","3000k","-bufsize","6000k",
+        "-vf","format=yuv420p,scale=1280:720","-r","30","-g","50",
+        "-c:a","aac","-b:a","128k","-ar","44100",
+        "-hide_banner",
+        #"-loglevel", "quiet",
+        "-f","flv", f"rtmp://a.rtmp.youtube.com/live2/{stream_key}"
     ])
